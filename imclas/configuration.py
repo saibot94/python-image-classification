036dc1d2370e391bf4496671fe868d44f7a64962
Cristi Schuszter <darkgeeky@gmail.com>
2016-06-01 23:52:00 +0300

    Moved library to separate folder

--- imclas/configuration.py
+++
@@ -1,2 +0,0 @@
-API_KEY = 'hbkeqj0wjrL1kIO0bE/s6z2I77jIr/jD+RV7O1fzr5I'
-DEFAULT_DB_LOCATION = "C:\\Users\\darkg\\Documents\\licenta\\python-image-classification\\db.sqlite3"
---
+++ python_library/imclas/configuration.py
@@ -0,0 +1,2 @@
+API_KEY = '<YOUR_API_KEY>'
+DEFAULT_DB_LOCATION = "C:\\Users\\darkg\\Documents\\licenta\\python-image-classification\\python_library\\db.sqlite3"
